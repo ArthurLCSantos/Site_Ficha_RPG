16d1563fa3d45ea26e0450266f4af3fe5be7c60f
ArthurLCSantos <catce.2022111isinf0546@aluno.ifpi.edu.br>
2026-05-26 10:17:37 -0300

    Consertado erro em delete da api de personarma.

--- src/app/api/personarma/delete/route.ts
+++ src/app/api/personarma/delete/route.ts
@@ -7,8 +7,8 @@ export async function DELETE(req : Request) {
 
     const personarma = await prisma.personarma.findUnique({where:{id}})
 
-    if (!personarma) return Response.json({error:"Personarma não encontrado"},{status:404})
-    if (personarma?.usuarioId !== session?.user?.role) return Response.json({error:"Não autorizado"},{status:403})
+    if (!personarma) return Response.json({error:"Personarma não encontrada"},{status:404})
+    if (personarma?.usuarioId !== session?.user?.id) return Response.json({error:"Não autorizado"},{status:403})
 
     await prisma.personarma.delete({where: {id}})
     
